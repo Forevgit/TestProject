bd1d9c8d27a8c352a63ecc007f132d034104ed1d
oleksandr <SaneXTRI@gmail.com>
2024-09-20 11:36:49 +0200

    add readmy

---
+++ README.md
@@ -0,0 +1,32 @@
+Інструкція по запуску проекту Task Management System
+
+1. Clone the Repository
+    git clone https://github.com/Forevgit/TestProject.git
+    cd TestProject
+
+2. Set Up the Environment Variables
+    Create a .env file in the root directory
+    #.env
+    DB_NAME=task_management
+    DB_USER=task_user
+    DB_PASSWORD=secret
+    DB_HOST=db
+    DB_PORT=5432
+    SECRET_KEY=your_secret_key
+    DEBUG=True
+
+3. Build and Run Docker Containers
+    docker-compose up --build
+
+4. Run Database Migrations
+    docker-compose exec web python manage.py migrate
+
+5. Create a Superuser
+    docker-compose exec web python manage.py createsuperuser
+
+6. Access the Project
+    Your web application should now be accessible at http://localhost:8000.
+
+API Endpoints:
+    You can use http://127.0.0.1:8000/swagger/ to see primary Endpoints
+
--- app/tasks/views.py
+++ app/tasks/views.py
@@ -6,6 +6,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.core.cache import cache
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 from rest_framework.response import Response
 
 class TaskViewSet(viewsets.ModelViewSet):
@@ -16,6 +18,16 @@ class TaskViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['status', 'priority', 'created_at']
     ordering_fields = ['created_at', 'updated_at']
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('status', openapi.IN_QUERY, description="Filter by task status", type=openapi.TYPE_STRING),
+            openapi.Parameter('priority', openapi.IN_QUERY, description="Filter by task priority", type=openapi.TYPE_STRING),
+            openapi.Parameter('created_at', openapi.IN_QUERY, description="Filter by task creation date", type=openapi.TYPE_STRING),
+            openapi.Parameter('ordering', openapi.IN_QUERY, description="Order by created_at or updated_at", type=openapi.TYPE_STRING),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
     def retrieve(self, request, *args, **kwargs):
         task_id = kwargs['pk']
